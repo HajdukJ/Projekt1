5bc2c172b4aa45d4a0fc0e2ad0a97077a9e58f7b
Jakub <Jakub@DESKTOP-E9K5LU2>
2024-10-16 17:27:04 +0200

    dodanie pozostalych funkcji i przetestowanie w main, nalezy dopisac metode usunzindeks

--- Projekt1/Projekt1.cpp
+++ Projekt1/Projekt1.cpp
@@ -12,8 +12,9 @@ class ListaDwukierunkowa {
 private:
     wezly* glowa;
     wezly* ogon;
+    wezly* obecny;
 public:
-    ListaDwukierunkowa(void): glowa(NULL), ogon(NULL) {}
+    ListaDwukierunkowa(void): glowa(NULL), ogon(NULL), obecny(NULL) {}
     ~ListaDwukierunkowa(void) {
         wyczysc();
     }
@@ -43,8 +44,32 @@ public:
         }
         ogon = element;
     }
-    void dodajNAindeks(void) {
-
+    void dodajNAindeks(int numer, int pozycja) {
+        if (pozycja  < 0) {
+            cout << "Blad." << endl;
+        }
+        if (pozycja == 0) {
+            dodajNApoczatek(numer);
+        }
+        wezly* obecny = glowa;
+        wezly* x = new wezly;
+        x->dane = numer;
+        int i = 0;
+        while (i < pozycja - 1 && obecny != NULL) {
+            obecny = obecny->nastepny;
+            i++;
+        }
+        if (obecny == NULL) {
+            dodajNAkoniec(numer);
+        }
+        else {
+            x->nastepny = obecny->nastepny;
+            x->poprzedni = obecny;
+            obecny->nastepny = x;
+            if (x->nastepny != NULL) {
+                x->nastepny->poprzedni = x;
+            }
+        }
     }
     void usunZpoczatek(void) {
         wezly* x = glowa;
@@ -73,7 +98,7 @@ public:
         }
     }
     void usunZindeks(void) {
-            
+        // dokonczyc
     }
     void wyswietl(void) {
         wezly* x;
@@ -93,10 +118,32 @@ public:
         cout << endl;
     }
     void wyswietlnastepny(void) {
-
+        if (obecny == NULL) {
+            obecny = glowa;
+        }
+        else {
+            if (obecny->nastepny != NULL) {
+                obecny = obecny->nastepny;
+            }
+            else {
+                cout << "Koniec listy, brak nastepnego." << endl;
+            }
+        }
+        cout << "Nastepny element to: " << obecny->dane << endl;
     }
     void wyswietlpoprzedni(void) {
-
+        if (obecny == NULL) {
+            cout << "Poczatek listy, brak poprzednika." << endl;
+            return;
+        }
+        wezly* x = obecny->poprzedni;
+        if (x != NULL) {
+            cout << "Poprzedni element to: " << x->dane << endl;
+            obecny = x;
+        }
+        else {
+            cout << "Poczatek listy, brak poprzednika." << endl;
+        }
     }
     void wyczysc(void) {
         wezly* x;
@@ -112,31 +159,25 @@ public:
 int main()
 {
     ListaDwukierunkowa lista;
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 8; i++) {
         lista.dodajNAkoniec(i);
     }
     lista.wyswietl();
     lista.usunZpoczatek();
-    lista.wyswietl();
     lista.usunZkoniec();
-    lista.wyswietl();
-    lista.dodajNAkoniec(8);
-    lista.wyswietl();
-    lista.dodajNApoczatek(3);
-    lista.wyswietl();
     /*
-    lista.dodajNApoczatek();
-    lista.dodajNAkoniec();
-    lista.dodajNAindeks();
-    lista.usunZpoczatek();
-    lista.usunZkoniec();
     lista.usunZindeks();
+    */
     lista.wyswietl();
-    lista.wyswietlodwrotnie();
+    lista.dodajNApoczatek(77);
+    lista.dodajNAkoniec(82);
+    lista.dodajNAindeks(52, 4);
+    lista.wyswietl();
+    lista.wyswietlnastepny();
+    lista.wyswietlnastepny();
     lista.wyswietlnastepny();
     lista.wyswietlpoprzedni();
-    lista.wyczysc();
-    */
+    lista.wyswietlpoprzedni();
     lista.wyswietlodwrotnie();
     lista.wyczysc();
     lista.wyswietl();
