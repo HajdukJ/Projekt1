0519275b15e789a291654a4e3e0b4a02ed1cf190
Jakub <Jakub@DESKTOP-E9K5LU2>
2024-10-21 18:36:48 +0200

    rozdzielenie klasy oraz main do osobnych plków

--- Projekt1/Projekt1.cpp
+++ Projekt1/Projekt1.cpp
@@ -182,30 +182,3 @@ public:
         glowa = NULL;
     }
 };
-
-int main()
-{
-    ListaDwukierunkowa lista;
-    for (int i = 1; i <= 8; i++) {
-        lista.dodajNAkoniec(i);
-    }
-    lista.wyswietl();
-    lista.usunZpoczatek();
-    lista.usunZkoniec();
-    lista.usunZindeks(3);
-    lista.wyswietl();
-    lista.dodajNApoczatek(77);
-    lista.dodajNAkoniec(82);
-    lista.dodajNAindeks(52, 4);
-    lista.wyswietl();
-    lista.wyswietlnastepny();
-    lista.wyswietlnastepny();
-    lista.wyswietlnastepny();
-    lista.wyswietlpoprzedni();
-    lista.wyswietlpoprzedni();
-    lista.wyswietlpoprzedni();
-    lista.wyswietlodwrotnie();
-    lista.wyczysc();
-    lista.wyswietl();
-    return 0;
-}
